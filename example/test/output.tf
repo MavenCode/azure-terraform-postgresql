433102203a90a02a303f6d4c122223621d2d66c2
KennedyUC <chukwukakennedyuche@gmail.com>
2022-02-11 12:45:38 -0800

    config scripts added

---
+++ example/test/output.tf
@@ -0,0 +1,15 @@
+output "server_name" {
+  value       = azurerm_postgresql_server.sql_server.name
+}
+
+output "server_fqdn" {
+  value       = azurerm_postgresql_server.sql_server.fqdn
+}
+
+output "server_id" {
+  value       = azurerm_postgresql_server.sql_server.id
+}
+
+output "database_ids" {
+  value       = [azurerm_postgresql_database.sql_db.*.id]
+}
